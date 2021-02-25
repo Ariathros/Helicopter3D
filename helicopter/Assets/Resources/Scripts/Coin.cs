bdaed8a0d76a818631ef208c9a5474b933b0da4b
Carlo Abalos <73739927+Ariathros@users.noreply.github.com>
2021-02-25 16:54:59 +0800

    Files

---
+++ helicopter/Assets/Resources/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		// trigger coin pickup function if a helicopter collides with this
+		other.transform.parent.GetComponent<HeliController>().PickupCoin(1);
+		Destroy(gameObject);
+	}
+}
---
+++ helicopter/Assets/Resources/Scripts/Gems.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gems : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        // trigger coin pickup function if a helicopter collides with this
+        other.transform.parent.GetComponent<HeliController>().PickupCoin(5);
+        Destroy(gameObject);
+    }
+
+}
